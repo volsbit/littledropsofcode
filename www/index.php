c0f6de653f7ca10aae8c9e00d3045638e9f4cad6
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-07-21 17:17:58 +0000

    Added CSS stylesheet.

--- www/index.php
+++ www/index.php
@@ -1,6 +1,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" version="XHTML 1.1">
   <head>
     <title>Little drops of code</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
   </head>
 
   <body>
